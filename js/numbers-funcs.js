8cc93e0b03256cd2df3f17ee640a92bef0100074
ChaniceStl <chanicestlouis2020@gmail.com>
2016-12-20 15:37:38 -0500

    part 1 mocha

--- js/numbers-funcs.js
+++ js/numbers-funcs.js
@@ -1,5 +1,42 @@
 const add = (a, b) => a + b;
+const even = arr => {
+	return arr.filter(element => element % 2 == 0)
+}
+const secondSmallest = (arr) => {
+	var smallest = Number.POSITIVE_INFINITY
+	var secSmall = arr[0]
+	for(var i = 0; i < arr.length; i++) {
+		if(smallest > arr[i]) {
+			secSmall = smallest
+			smallest = arr[i]
+		}
+	}
+	return secSmall
+}
+
+const uniqueNum = arr =>{
+	var hash = {}
+	var newArr = []
+	for(var i = 0; i< arr.length; i++){
+		if(hash[arr[i]]){
+			hash[arr[i]] +=1
+		}
+			else {
+				hash[arr[i]] = 1
+			}
+	}
+  for(var key in hash){
+   console.log(hash[key])
+   if(hash[key] === 1){
+     newArr.push(parseInt(key))
+   }
+  }
+  return newArr;
+}
 
 module.exports = {
-  add: add
+  add: add,
+  even: even,
+  secondSmallest: secondSmallest,
+  uniqueNum:uniqueNum
 };
--- test/numbers-tests.js
+++ test/numbers-tests.js
@@ -1,5 +1,10 @@
 const expect = require('chai').expect;
 const add = require('../js/numbers-funcs').add;
+const even = require('../js/numbers-funcs').even;
+const secondSmallest = require('../js/numbers-funcs').secondSmallest;
+const uniqueNum = require('../js/numbers-funcs').uniqueNum;
+
+
 
 describe('numbers tests', () => {
   it('add() should add two numbers', () => {
@@ -10,5 +15,26 @@ describe('numbers tests', () => {
     expect(result2).equal(16);
     expect(result3).equal(23);
   });
-  //Add more tests here:
+  it('even() should give all even numbers', () => {
+  	var result1 = even([1,2,3,4,5,6]);
+  	var result2= even([2,4,6,8,10]);
+  	var result3 = even([3,5,7,9,11])
+  	expect(result1).to.eql([2,4,6])
+  	expect(result2).to.eql([2,4,6,8,10])
+  	expect(result3).to.eql([])
+  });
+  it('secondSmallest() should return the second smallest number', () => {
+  	var result1 = secondSmallest([34, 1, 43, -32, 9]);
+  	var result2 = secondSmallest([26,88,3,99]);
+  	expect(result1).to.eql(1);
+  	expect(result2).to.eql(26);
+  })
+  it("uniqueNum() should return the unique number", () => {
+  		var result1 = uniqueNum([1,2,3,3,5,6]);
+  		var result2= uniqueNum([2,4,6,8,10]);
+  		expect(result1).to.eql([1,2,5,6]);
+  		expect(result2).to.eql([2,4,6,8,10]);
+  })
+
+
 });
